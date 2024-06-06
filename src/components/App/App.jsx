c96d2fb499a34d5b419a44875ba60f326604242b
chef_dill <dillon.sareerat@gmail.com>
2024-06-06 17:42:21 -0500

    review

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
 import "./App.css";
 import Header from "../Header/Header";
+import Reviews from "../Reviews/Reviews";
+import AboutMe from "../AboutMe/AboutMe";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/about-me" element={<AboutMe />} />
+      </Routes>
     </div>
   );
 }
